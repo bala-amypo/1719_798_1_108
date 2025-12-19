7111784d10ffd097b7f22a02eceb345a6093f439
bala-amypo <bala@amypo.in>
2025-12-19 07:49:23 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class OpenApi {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9092.408procr.amypo.ai/")
+                        new Server().url("https://9099.408procr.amypo.ai/")
                 ));
         }
 }
