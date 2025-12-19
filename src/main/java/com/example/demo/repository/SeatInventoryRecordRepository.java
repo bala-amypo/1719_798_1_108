c44056807d5e4b61fd0acb9a4b7afdc1d867ff5e
bala-amypo <bala@amypo.in>
2025-12-19 09:16:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatInventoryRecordRepository.java
+++ src/main/java/com/example/demo/repository/SeatInventoryRecordRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SeatInventoryRecordRepository
-        extends JpaRepository<SeatInventoryRecord, Long> {
+public interface SeatInventoryRecordRepository extends JpaRepository<SeatInventoryRecord, Long> {
 
+    // Spring Data JPA will resolve this automatically by the "event" field in SeatInventoryRecord
     Optional<SeatInventoryRecord> findByEventId(Long eventId);
 }
