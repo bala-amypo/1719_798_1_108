7ad227b6aa04e81df3307436e50e60422fbff877
bala-amypo <bala@amypo.in>
2025-12-19 06:47:12 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -2,17 +2,23 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI securityOpenAPI() {  // renamed from customOpenAPI
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                // Server info
+                .servers(List.of(new Server().url("https://9099.408procr.amypo.ai/")))
+                // Security scheme
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
@@ -22,29 +28,3 @@ public class OpenApiConfig {
                                         .bearerFormat("JWT")));
     }
 }
-
-
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.Components;
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class OpenApiConfig {
-
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-//                 .components(new Components()
-//                         .addSecuritySchemes("bearerAuth",
-//                                 new SecurityScheme()
-//                                         .type(SecurityScheme.Type.HTTP)
-//                                         .scheme("bearer")
-//                                         .bearerFormat("JWT")));
-//     }
-// }
