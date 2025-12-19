6c193dd6a8e19a49fd5bedc094dc1eae0ae3fa57
bala-amypo <bala@amypo.in>
2025-12-19 08:37:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/SeatInventoryRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.model.SeatInventoryRecord;
+
+import java.util.List;
+
+public interface SeatInventoryService {
+
+    SeatInventoryRecord createInventory(SeatInventoryRecord inventory);
+
+    SeatInventoryRecord updateRemainingSeats(Long eventId, Integer remainingSeats);
+
+    SeatInventoryRecord getInventoryByEvent(Long eventId);
+
+    List<SeatInventoryRecord> getAllInventories();
+
+    SeatInventoryRecord getById(Long id);
+}
