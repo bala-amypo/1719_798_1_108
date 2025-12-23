84f520c35c812aa28fc5778529c1cf08d3188229
bala-amypo <bala@amypo.in>
2025-12-23 08:56:37 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -8,7 +8,7 @@ import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     
-    // Change from protected to public
+    
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
