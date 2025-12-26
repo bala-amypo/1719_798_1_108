2db824a0a4222d10c52454b7b5edb512e4090d96
bala-amypo <bala@amypo.in>
2025-12-26 14:21:01 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,6 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
 
@@ -18,32 +16,3 @@ public class DemoApplication {
         }
     }
 }
-
-
-// package com.example.demo;
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.web.servlet.ServletRegistrationBean;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import com.example.demo.servlet.HelloServlet;
-// @SpringBootApplication
-// public class DemoApplication {
-//     public static void main(String[] args) {
-//         SpringApplication.run(DemoApplication.class, args);
-//         System.out.println("Dynamic Event Ticket Pricing Engine Application Started!");
-//         System.out.println("Application is running...");
-//     }
-//     @Bean
-//     public ServletRegistrationBean<HelloServlet> helloServlet() {
-//         ServletRegistrationBean<HelloServlet> registrationBean = 
-//                 new ServletRegistrationBean<>(new HelloServlet(), "/hello");
-//         registrationBean.setLoadOnStartup(1);
-//         registrationBean.setName("helloServlet");
-//         return registrationBean;
-//     }
-//     @Bean
-//     public BCryptPasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
